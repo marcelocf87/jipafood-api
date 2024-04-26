4503e81e92fe9ddb727fc5a0155c7f1f3fb109cf
Marcelo <marceloo.cfreitas@gmail.com>
2024-04-26 10:22:11 -0400

    Iniciando o projeto da API

---
+++ src/main/java/com/marcelocf87/jipafood/JipafoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.marcelocf87.jipafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JipafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JipafoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelocf87/jipafood/JipafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelocf87.jipafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JipafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
